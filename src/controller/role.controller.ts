d7848915bbac8bd439aa207a9c74f59cca2816d1
Randolph Schreiner <Rschreiner83@gmail.com>
2023-01-14 12:48:18 -0500

    roleCrudComplete

--- src/controller/role.controller.ts
+++ src/controller/role.controller.ts
@@ -1,4 +1,5 @@
 import {Request, Response} from "express";
+import { Entity } from "typeorm";
 import { dataSource } from "../data-source";
 import {Role} from "../entity/role.entity";
 
@@ -18,12 +19,39 @@ export const CreateRole = async (req: Request, res: Response) => {
         permissions: permissions.map(id => ({id}))
     });
     
-    res.send(role);
+    res.status(201).send(role);
 }
 
 export const GetRole = async (req: Request, res: Response) => {
     const repository = dataSource.getRepository(Role);
     
-    res.send(await repository.findOne(req.params.id, {relations: ['permissions']}))
-}
-//relations not working 
+    res.send(await repository.findOne({
+        where: {id : parseInt(req.params.id)},
+        relations:{
+            permissions: true,
+        }
+    }))};
+
+    export const UpdateRole = async (req: Request, res: Response) => {
+        const {name, permissions} = req.body;
+        
+        const repository = dataSource.getRepository(Role);
+
+        const role = await repository.save({
+            id: parseInt(req.params.id),
+            name,
+            permissions: permissions.map(id => ({id}))
+        });
+
+        res.status(202).send(role);
+
+    }
+
+    export const DeleteRole = async (req:Request, res: Response) => {
+        const repository = dataSource.getRepository(Role);
+
+        await repository.delete(req.params.id);
+
+        res.status(204).send(null);
+
+    }
--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,8 @@ import {Router} from "express";
 import {Register, Login, Logout, AuthenticatedUser, UpdateInfo, UpdatePassword} from "./controller/auth.controller"
 import { AuthMiddleware } from "./middleware/auth.middleware";
 import {Users, CreateUser, GetUser, UpdateUser, DeleteUser} from "./controller/user.controller";
-import { DataTypeNotSupportedError } from "typeorm";
 import {Permissions} from "./controller/permission.controller";
-import {CreateRole, Roles, GetRole} from "./controller/role.controller";
+import {CreateRole, Roles, GetRole, UpdateRole, DeleteRole} from "./controller/role.controller";
 
 
 export const routes = (router: Router) => {
@@ -26,4 +25,6 @@ export const routes = (router: Router) => {
    router.get('/api/roles', AuthMiddleware, Roles);
    router.post('/api/roles', AuthMiddleware, CreateRole);
    router.get('/api/roles/:id', AuthMiddleware, GetRole);
+   router.put('/api/roles/:id', AuthMiddleware, UpdateRole);
+   router.delete('/api/roles/:id', AuthMiddleware, DeleteRole);
 }
